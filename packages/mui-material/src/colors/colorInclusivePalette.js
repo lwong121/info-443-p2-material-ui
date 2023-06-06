eb72b3852332ea634d7579229148a2b96ac59f55
sachifig <sachifig@uw.edu>
2023-06-06 13:07:58 -0700

    added colorInclusivePalette

---
+++ packages/mui-material/src/colors/colorInclusivePalette.js
@@ -0,0 +1,32 @@
+export const colorBlindnessPalette = {
+    primary: '#007bff',
+    secondary: '#6c757d',
+    error: '#dc3545'
+  };
+  
+  // Color palettes for ed-green color blindness
+  export const protanopiaPalette = {
+    primary: '#0033cc',
+    secondary: '#888888',
+    error: '#cc0000'
+  };
+  
+  export const deuteranopiaPalette = {
+    primary: '#0066cc',
+    secondary: '#888888',
+    error: '#cc0000'
+  };
+  
+  // Color palette for lue-yellow color blindness
+  export const tritanopiaPalette = {
+    primary: '#ff0000',
+    secondary: '#0000ff',
+    error: '#ff6600'
+  };
+
+  export default {
+    colorBlindnessPalette,
+    protanopiaPalette,
+    deuteranopiaPalette,
+    tritanopiaPalette,
+  };
